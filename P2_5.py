bf0c4487abd62db5fadbfb6da8c736163ce8b181
laialanza <laiaipatch@hotmail.com>
2021-11-17 15:54:09 +0100

    Ex5 Updated

--- P2_5.py
+++ P2_5.py
@@ -45,20 +45,23 @@ while answer:
         video = input(
             "Introduce the directory of the video you want to cut: \n ")
         size = int(
-            input("PUT 1 if you want 720p \n 2 if you want 480p\n 3 if you want "
+            input("INTRODUCE: \n 1 if you want 720p \n 2 if you want 480p\n 3 if you want "
                   "360x240 \n or anything else if you want 160x120: \n "))
 
         clip = mp.VideoFileClip(video)
         if size == 1:
             clip_resized = clip.resize(newsize=(1280, 720))
-        if size == 2:
+            clip_resized.write_videofile("resized_video_720p.mp4")
+        elif size == 2:
             clip_resized = clip.resize(newsize=(852, 480))
-        if size == 3:
+            clip_resized.write_videofile("resized_video_480p.mp4")
+        elif size == 3:
             clip_resized = clip.resize(newsize=(360, 240))
-        else:
+            clip_resized.write_videofile("resized_video_360x240.mp4")
+        elif size == 4:
             clip_resized = clip.resize(newsize=(160, 120))
+            clip_resized.write_videofile("resized_video_160x120.mp4")
 
-        clip_resized.write_videofile("resized_video.mp4")
     #Ex 4
     elif answer == 4:
         video = input(
