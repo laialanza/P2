e935f63ab482c7916a95c3f3e2b6447d5ac48643
laialanza <laiaipatch@hotmail.com>
2021-11-17 15:23:06 +0100

    Update

--- P2_2.py
+++ P2_2.py
@@ -1,4 +1,5 @@
 import subprocess
-subprocess.call(["ffmpeg", "-i", "cut_video.mp4", "-vf",
+video = input("Introduce the directory of the video you want to cut: \n ")
+subprocess.call(["ffmpeg", "-i", video, "-vf",
                  "split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay",
                  "output_yuv.mp4"])
